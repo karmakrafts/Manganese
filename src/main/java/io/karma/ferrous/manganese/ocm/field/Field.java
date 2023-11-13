3a7875521083cbc9bb1eff063bdc497304a10aba
KitsuneAlex <alexh@karmakrafts.dev>
2023-11-14 00:18:39 +0100

    Move Field to field package and implement FieldValueStorage

---
+++ src/main/java/io/karma/ferrous/manganese/ocm/field/Field.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2023 Karma Krafts & associates
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.karma.ferrous.manganese.ocm.field;
+
+import io.karma.ferrous.manganese.ocm.NameProvider;
+import io.karma.ferrous.manganese.ocm.access.Access;
+import io.karma.ferrous.manganese.ocm.access.AccessProvider;
+import io.karma.ferrous.manganese.ocm.scope.Scope;
+import io.karma.ferrous.manganese.ocm.scope.Scoped;
+import io.karma.ferrous.manganese.ocm.type.Type;
+import io.karma.ferrous.manganese.util.Identifier;
+import io.karma.ferrous.manganese.util.StorageMod;
+import io.karma.ferrous.manganese.util.TokenSlice;
+import org.apiguardian.api.API;
+import org.apiguardian.api.API.Status;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.EnumSet;
+
+/**
+ * @author Alexander Hinze
+ * @since 15/10/2023
+ */
+@API(status = Status.INTERNAL)
+public final class Field implements NameProvider, AccessProvider, Scoped {
+    private final int index;
+    private final Identifier name;
+    private final Type type;
+    private final Access access;
+    private final TokenSlice tokenSlice;
+    private final EnumSet<StorageMod> storageMods;
+    private final boolean isMutable;
+    private final boolean isStatic;
+    private final boolean isGlobal;
+    private Scope enclosingScope;
+
+    public Field(final int index, final Identifier name, final Type type, final Access access,
+                 final EnumSet<StorageMod> storageMods, final boolean isMutable, final boolean isStatic,
+                 final boolean isGlobal, final TokenSlice tokenSlice) {
+        this.index = index;
+        this.name = name;
+        this.type = type;
+        this.access = access;
+        this.storageMods = storageMods;
+        this.isMutable = isMutable;
+        this.isStatic = isStatic;
+        this.isGlobal = isGlobal;
+        this.tokenSlice = tokenSlice;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public EnumSet<StorageMod> getStorageMods() {
+        return storageMods;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public TokenSlice getTokenSlice() {
+        return tokenSlice;
+    }
+
+    public boolean isStatic() {
+        return isStatic;
+    }
+
+    public boolean isMutable() {
+        return isMutable;
+    }
+
+    public boolean isGlobal() {
+        return isGlobal;
+    }
+
+    // AccessProvider
+
+    @Override
+    public Access getAccess() {
+        return access;
+    }
+
+    // Scoped
+
+    @Override
+    public @Nullable Scope getEnclosingScope() {
+        return enclosingScope;
+    }
+
+    @Override
+    public void setEnclosingScope(final Scope enclosingScope) {
+        this.enclosingScope = enclosingScope;
+    }
+
+    // NameProvider
+
+    @Override
+    public Identifier getName() {
+        return name;
+    }
+
+    // Object
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s", name, type);
+    }
+}
---
+++ src/main/java/io/karma/ferrous/manganese/ocm/field/FieldValueStorage.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2023 Karma Krafts & associates
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.karma.ferrous.manganese.ocm.field;
+
+import io.karma.ferrous.manganese.ocm.ValueStorage;
+import io.karma.ferrous.manganese.ocm.expr.Expression;
+import io.karma.ferrous.manganese.ocm.ir.IRContext;
+import io.karma.ferrous.manganese.ocm.type.Type;
+import io.karma.ferrous.manganese.ocm.type.UDT;
+import io.karma.ferrous.manganese.target.TargetMachine;
+import io.karma.ferrous.manganese.util.Identifier;
+import org.apiguardian.api.API;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Alexander Hinze
+ * @since 10/11/2023
+ */
+@API(status = API.Status.INTERNAL)
+public final class FieldValueStorage implements ValueStorage, FieldStorageProvider {
+    private final Field field;
+    private final FieldStorageProvider parent;
+    private final List<FieldValueStorage> fieldValues;
+    private Expression value;
+    private boolean hasChanged;
+    private boolean isInitialized;
+
+    public FieldValueStorage(final Field field, final FieldStorageProvider parent) {
+        this.field = field;
+        this.parent = parent;
+        isInitialized = parent.isInitialized();
+        if (isInitialized) {
+            value = field.getType().makeDefaultValue();
+        }
+        if (field.getType() instanceof UDT udt) {
+            fieldValues = udt.fields().stream().map(f -> new FieldValueStorage(f, this)).toList();
+        }
+        else {
+            fieldValues = Collections.emptyList();
+        }
+    }
+
+    // ValueCarrier
+
+    @Override
+    public void setInitialized() {
+        isInitialized = true;
+    }
+
+    @Override
+    public boolean isInitialized() {
+        return isInitialized;
+    }
+
+    @Override
+    public Identifier getName() {
+        return field.getName();
+    }
+
+    @Override
+    public FieldStorageProvider getParent() {
+        return parent;
+    }
+
+    // FieldStorageProvider
+
+    @Override
+    public Type getLoadType() {
+        return field.getType();
+    }
+
+    @Override
+    public List<FieldValueStorage> getFieldValues() {
+        return fieldValues;
+    }
+
+    // ValueStorage
+
+    @Override
+    public void notifyChanged() {
+        hasChanged = true;
+    }
+
+    @Override
+    public @Nullable Expression getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean isMutable() {
+        return field.isMutable();
+    }
+
+    @Override
+    public boolean hasChanged() {
+        return hasChanged;
+    }
+
+    @Override
+    public long getAddress(final TargetMachine targetMachine, final IRContext irContext) {
+        final var builder = irContext.getCurrentOrCreate();
+        final var typeAddress = Objects.requireNonNull(parent.getLoadType()).materialize(targetMachine);
+        return builder.gep(typeAddress, parent.getAddress(targetMachine, irContext), field.getIndex());
+    }
+
+    @Override
+    public long storeAddress(final @Nullable Expression exprValue, final long address,
+                             final TargetMachine targetMachine, final IRContext irContext) {
+        this.value = exprValue;
+        return FieldStorageProvider.super.storeAddress(exprValue, address, targetMachine, irContext);
+    }
+
+    @Override
+    public long store(final @Nullable Expression exprValue, final long value, final TargetMachine targetMachine,
+                      final IRContext irContext) {
+        this.value = exprValue;
+        return FieldStorageProvider.super.store(exprValue, value, targetMachine, irContext);
+    }
+}
